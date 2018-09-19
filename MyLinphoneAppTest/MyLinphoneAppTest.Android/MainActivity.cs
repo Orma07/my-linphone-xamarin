63fe7f2447d2e1dc3914cc412f9b231f21fbf609
Amro Abd Elgawwad <amro.abdelgawwad@kalpa.it>
2018-09-19 15:25:24 +0200

    beamforming test 2

--- MyLinphoneAppTest/MyLinphoneAppTest.Android/MainActivity.cs
+++ MyLinphoneAppTest/MyLinphoneAppTest.Android/MainActivity.cs
@@ -7,6 +7,7 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using LibLinphone.Droid.LinphoneUtils;
+using Java.IO;
 
 namespace MyLinphoneAppTes.Droid
 {
@@ -25,8 +26,29 @@ namespace MyLinphoneAppTes.Droid
             base.OnCreate(bundle);
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LinphoneManagerAndroid.Start();
-            LoadApplication(new App());
-            
+            LoadApplication(new App()); 
+
+            var folder = new File("/certs/");
+
+            var files = folder.ListFiles();
+
+            File certificate = null;
+
+            if (files != null)
+            {
+                foreach (var fileEntry in files)
+                {
+                    System.Diagnostics.Debug.WriteLine("Certificate name: " + fileEntry.Name);
+                    System.Diagnostics.Debug.WriteLine("Certificate can read: " + fileEntry.CanRead().ToString());
+
+
+                }
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("certs is null");
+            }
+
         }
     }
 }
