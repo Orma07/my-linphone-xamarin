e67d6b04fae160a292ee42f1e77e37b83b9611cf
enrico.sallusti <enrico.sallusti@kalpa.it>
2019-01-22 12:19:16 +0100

    Switched GL2JNIView with TextureView

--- LibLinphone.Android/LinphoneUtils/LinphoneVideoViewAndroid.cs
+++ LibLinphone.Android/LinphoneUtils/LinphoneVideoViewAndroid.cs
@@ -2,26 +2,25 @@ using System;
 
 using Android.App;
 using Android.Content;
-using Android.Media;
+using Android.Graphics;
 using Android.Runtime;
 using Android.Util;
 using Android.Views;
 using LibLinphone.Android.LinphoneUtils;
 using LibLinphone.Droid.LinphoneUtils;
 using LibLinphone.Views;
-using Org.Linphone.Mediastream.Video;
 using Xamarin.Forms.Platform.Android;
 
 [assembly: Xamarin.Forms.ExportRenderer(typeof(LinphoneVideoView), typeof(LinphoneVideoViewAndroid))]
 namespace LibLinphone.Droid.LinphoneUtils
 {
-    public class LinphoneVideoViewAndroid : ViewRenderer<LinphoneVideoView, View>
+    public class LinphoneVideoViewAndroid : ViewRenderer<LinphoneVideoView, View>, TextureView.ISurfaceTextureListener
     {
-        Org.Linphone.Mediastream.Video.Display.GL2JNIView captureCamera;
+        TextureView captureCamera;
         //SurfaceView captureCamera;
-        AndroidVideoWindowImpl androidView;
-        private MediaRecorder mMediaRecorder;
-        private AndroidVideoWindowListener androidVideoWindowListener;
+        //AndroidVideoWindowImpl androidView;
+        //private MediaRecorder mMediaRecorder;
+        //private AndroidVideoWindowListener androidVideoWindowListener;
         public LinphoneVideoViewAndroid(Context context) : base(context)
         {
 
@@ -31,74 +30,81 @@ namespace LibLinphone.Droid.LinphoneUtils
 
         private void InitAndroidView(int width, int height)
         {
-            captureCamera = new Org.Linphone.Mediastream.Video.Display.GL2JNIView(Context);
-
             var displayMetrics = new DisplayMetrics();
             var ctx = Application.Context;
             var windowManager = ctx.GetSystemService(Context.WindowService).JavaCast<IWindowManager>();
             windowManager.DefaultDisplay.GetMetrics(displayMetrics);
 
-            int width_android = (int)(width / (1f / displayMetrics.Density));
-            int height_android = (int)(height / (1f / displayMetrics.Density));
-            captureCamera.Holder.SetFixedSize(width_android, height_android);
-            ViewGroup.LayoutParams cparams = new ViewGroup.LayoutParams(width_android, height_android);
+            var widthAndroid = (int)(width / (1f / displayMetrics.Density));
+            var heightAndroid = (int)(height / (1f / displayMetrics.Density));
+            var cparams = new LayoutParams(widthAndroid, heightAndroid);
+            
+            captureCamera = new TextureView(Context) 
+            {
+                SurfaceTextureListener = this,
+            };
+            captureCamera.SetMinimumWidth(widthAndroid);
+            captureCamera.SetMinimumHeight(heightAndroid);    
             captureCamera.LayoutParameters = cparams;
+            //captureCamera.Holder.SetFixedSize(width_android, height_android);
+            
+            LinphoneEngineAndroid.Instance.LinphoneCore.NativeVideoWindowId = captureCamera.Handle;
+            
+            //androidView = new AndroidVideoWindowImpl(captureCamera, null, null);
 
-            androidView = new AndroidVideoWindowImpl(captureCamera, null, null);
-
-            androidVideoWindowListener = new AndroidVideoWindowListener();
-            androidVideoWindowListener.OnVideoRenderingSurfaceReadyEvent += OnVideoRenderingSurfaceReady;
-            androidVideoWindowListener.OnVideoRenderingSurfaceDestroyedEvent += OnVideoRenderingSurfaceDestroyed;
+            //androidVideoWindowListener = new AndroidVideoWindowListener();
+            //androidVideoWindowListener.OnVideoRenderingSurfaceReadyEvent += OnVideoRenderingSurfaceReady;
+           // androidVideoWindowListener.OnVideoRenderingSurfaceDestroyedEvent += OnVideoRenderingSurfaceDestroyed;
 
 
-            androidView.SetListener(androidVideoWindowListener);
+            //androidView.SetListener(androidVideoWindowListener);
 
-            captureCamera.SetZOrderOnTop(false);
-            captureCamera.SetZOrderMediaOverlay(true);
+            //captureCamera.SetZOrderOnTop(false);
+            //captureCamera.SetZOrderMediaOverlay(true);
            
         }
 
-        private void OnVideoRenderingSurfaceDestroyed(object sender, AndroidVideoWindowListenerArgs e)
+        /*private void OnVideoRenderingSurfaceDestroyed(object sender, AndroidVideoWindowListenerArgs e)
         {
             try
             {
-                androidView.Release();
-                androidView.Dispose();
+                //androidView.Release();
+                //androidView.Dispose();
             }
             catch
             {
 
             }
-        }
+        }*/
 
-        private void OnVideoRenderingSurfaceReady(object sender, AndroidVideoWindowListenerArgs e)
-        {
-            captureCamera = e.surfaceView as Org.Linphone.Mediastream.Video.Display.GL2JNIView;
-            if (LinphoneEngineAndroid.Instance.LinphoneCore.NativeVideoWindowId == IntPtr.Zero)
-            {
-                captureCamera = e.surfaceView as Org.Linphone.Mediastream.Video.Display.GL2JNIView;
-                LinphoneEngineAndroid.Instance.LinphoneCore.NativeVideoWindowId = e.androidVideoWindowImpl.Handle;
-            }
-        }
+//        private void OnVideoRenderingSurfaceReady(object sender, AndroidVideoWindowListenerArgs e)
+//        {
+//            captureCamera = e.surfaceView as Org.Linphone.Mediastream.Video.Display.GL2JNIView;
+//            if (LinphoneEngineAndroid.Instance.LinphoneCore.NativeVideoWindowId == IntPtr.Zero)
+//            {
+//                captureCamera = e.surfaceView as Org.Linphone.Mediastream.Video.Display.GL2JNIView;
+//                LinphoneEngineAndroid.Instance.LinphoneCore.NativeVideoWindowId = e.androidVideoWindowImpl.Handle;
+//            }
+//        }
 
-        protected override void OnDetachedFromWindow()
-        {
-            base.OnDetachedFromWindow();
-            if(LinphoneEngineAndroid.Instance.LinphoneCore.NativeVideoWindowId != IntPtr.Zero)
-            {
-                LinphoneEngineAndroid.Instance.LinphoneCore.NativeVideoWindowId = IntPtr.Zero;
-            }
+//        protected override void OnDetachedFromWindow()
+//        {
+//            base.OnDetachedFromWindow();
+//            if(LinphoneEngineAndroid.Instance.LinphoneCore.NativeVideoWindowId != IntPtr.Zero)
+//            {
+//                LinphoneEngineAndroid.Instance.LinphoneCore.NativeVideoWindowId = IntPtr.Zero;
+//            }
+//
+//        }
 
-        }
-
-        protected override void OnAttachedToWindow()
-        {
-            base.OnAttachedToWindow();
-            if (LinphoneEngineAndroid.Instance.LinphoneCore.NativeVideoWindowId == IntPtr.Zero)
-            {
-               // LinphoneEngineAndroid.Instance.LinphoneCore.NativeVideoWindowId = androidView.Handle;
-            }
-        }
+//        protected override void OnAttachedToWindow()
+//        {
+//            base.OnAttachedToWindow();
+//            if (LinphoneEngineAndroid.Instance.LinphoneCore.NativeVideoWindowId == IntPtr.Zero)
+//            {
+//               // LinphoneEngineAndroid.Instance.LinphoneCore.NativeVideoWindowId = androidView.Handle;
+//            }
+//        }
 
         private Xamarin.Forms.StackLayout s;
         private void PopulateView()
@@ -118,12 +124,10 @@ namespace LibLinphone.Droid.LinphoneUtils
             {
                 InitAndroidView((int)Element.Width, (int)Element.Height);
 
-                s = new Xamarin.Forms.StackLayout();
-                s.IsEnabled = false;
-                s.InputTransparent = true;
+                s = new Xamarin.Forms.StackLayout {IsEnabled = false, InputTransparent = true};
                 Element.InputTransparent = true;
-                Element.ZoomEvent -= OnZoomView;
-                Element.ZoomEvent += OnZoomView;
+//                Element.ZoomEvent -= OnZoomView;
+//                Element.ZoomEvent += OnZoomView;
                 Element.IsEnabled = false;
                 s.HorizontalOptions = Xamarin.Forms.LayoutOptions.FillAndExpand;
                 s.VerticalOptions = Xamarin.Forms.LayoutOptions.FillAndExpand;
@@ -133,25 +137,52 @@ namespace LibLinphone.Droid.LinphoneUtils
             }
         }
 
-        private void OnZoomView(object sender, ZoomEventsArg e)
-        {
+//        private void OnZoomView(object sender, ZoomEventsArg e)
+//        {
             //TODO: https://gitlab.linphone.org/BC/public/linphone-android/blob/master/src/android/org/linphone/call/CallVideoFragment.java
-        }
+//        }
 
         protected override void OnElementChanged(ElementChangedEventArgs<LinphoneVideoView> e)
         {
             base.OnElementChanged(e);
             if (e.NewElement != null)
-            {
                 PopulateView();
-            }
-
         }
 
-       
+        public void OnSurfaceTextureAvailable(SurfaceTexture surface, int width, int height)
+        {
+            if (LinphoneEngineAndroid.Instance.LinphoneCore.NativeVideoWindowId == IntPtr.Zero)
+                LinphoneEngineAndroid.Instance.LinphoneCore.NativeVideoWindowId = surface.Handle;
+        }
+
+        public bool OnSurfaceTextureDestroyed(SurfaceTexture surface)
+        {
+            try
+            {
+                if (LinphoneEngineAndroid.Instance.LinphoneCore.NativeVideoWindowId != IntPtr.Zero)
+                    LinphoneEngineAndroid.Instance.LinphoneCore.NativeVideoWindowId = IntPtr.Zero;
+
+                surface.Dispose();
+            }
+            catch
+            {
+                //We do not want the app to crash randomly
+            }
+            return true;
+        }
+
+        public void OnSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnSurfaceTextureUpdated(SurfaceTexture surface)
+        {
+            //throw new NotImplementedException();
+        }
     }
 
-    public class AndroidVideoWindowListenerArgs: EventArgs
+    /*public class AndroidVideoWindowListenerArgs: EventArgs
     {
         public AndroidVideoWindowImpl androidVideoWindowImpl { get;  set; }
         public SurfaceView surfaceView { get;  set; }
@@ -192,5 +223,5 @@ namespace LibLinphone.Droid.LinphoneUtils
                     surfaceView = p1
                 });
         }
-    }
+    }*/
 }
