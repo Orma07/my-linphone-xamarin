b363240e82d338e8a14d3849a3f79636b7a62f60
enrico.sallusti <enrico.sallusti@kalpa.it>
2019-03-22 09:47:24 +0100

    Deleted unused using imports

--- LibLinphone.Android/LinphoneUtils/LinphoneEngineAndroid.cs
+++ LibLinphone.Android/LinphoneUtils/LinphoneEngineAndroid.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
 using Android;
 using Android.App;
 using Android.Content;
